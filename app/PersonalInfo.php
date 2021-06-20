4fed3a7f4896fe175c1ad8ccc6623f911adc6ce7
champaksworld <champaksworld@users.noreply.github.com>
2021-06-20 20:59:48 +0530

    Platform Added

--- app/PersonalInfo.php
+++ app/PersonalInfo.php
@@ -7,16 +7,16 @@ use Illuminate\Database\Eloquent\Model;
 class PersonalInfo extends Model
 {
     //
-	protected $fillable = ['id','financialyear','firstname','lastname','mobileno','dob','emailid','panno','aadharno','userid','sourceofincome',];
+	protected $fillable = ['id','financialyear','firstname','lastname','mobileno','dob','emailid','panno','aadharno','userid','sourceofincome','platform',];
 	 protected $table = 'personalinfo';
 	 protected $primaryKey= 'id';
 	 const CREATED_AT = 'creation_date';
     const UPDATED_AT = 'updated_date';
 	
-	  public function user()
+	 /* public function user()
     {
         return $this->hasOne(User::class.'');
-    }
+    }*/
 	
 	
 }
